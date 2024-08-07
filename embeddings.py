d1b518da4c3b826fa0f69e1499a36db75d6a5c99
Eduarda Alves <95770766+Dudexmaverick@users.noreply.github.com>
2024-08-07 14:33:10 -0300

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -3,7 +3,7 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import Ollama
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate,SystemMessagePromptTemplate,HumanMessagePromptTemplate
 
 def create_vectorstores(chunks):
 
@@ -16,19 +16,31 @@ def create_vectorstores(chunks):
 def create_conversation_chain(vectorstore):
 
     llm = Ollama(model="llama3",temperature= 0.1) 
-    
+    general_system_template = """
+    Você é um assistente virtual, que responde exclusivamente em português sobre os conteúdos dos PDFs armazenados.
+    Use o contexto fornecido para responder à pergunta de forma clara e concisa.
+    Se e pergunta for fora do contexto dos PDFs, responda que não pode responder fora do tópico.:
 
+    {context}
+
+    ---
+
+    Answer the question based on the above context: {question}
+    """
+    general_user_template = "Question:"""
+    messages = [
+        SystemMessagePromptTemplate.from_template(general_system_template), 
+        HumanMessagePromptTemplate.from_template(general_user_template)
+    ]
+    qa_prompt = ChatPromptTemplate.from_messages (messages)
+    
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
 
 
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
         retriever= vectorstore.as_retriever(),
-        memory=memory
+        memory=memory, 
+        combine_docs_chain_kwargs={'prompt': qa_prompt}
     )
-       
-    
-
-
     return conversation_chain
- 
